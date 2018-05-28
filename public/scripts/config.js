d5710a6d2ac06bad3a697271746b622c16bfc75d
Klaas-Andries Hoekstra <klaasandrieshoekstra@gmail.com>
2018-05-28 10:39:11 +0200

    first commit

---
+++ public/scripts/config.js
@@ -0,0 +1,12 @@
+/* 
+*  Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+*  See LICENSE in the source repository root for complete license information. 
+*/
+
+var APPLICATION_CONFIG = {
+    clientID: "dcff05af-06b4-4c33-b302-7b07f68ba531",
+    redirectUri: "http://localhost:8080/",
+    interactionMode: "popUp",
+    graphEndpoint: "https://graph.microsoft.com/v1.0/me",
+    graphScopes: ["user.read user.read.all user.readbasic.all"]
+};
